aa0f38c821e840201870a4f05ee0399ee5abe3da
Alezuna Nadhif <110968215+AlezunaNadhif@users.noreply.github.com>
2023-06-17 11:06:27 +0700

    update

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -2,18 +2,17 @@
     <nav>
         <div id="flex">
             @if(Auth::check())
-            <h3>Halo kak, {{ Auth::user()->name }}</h3>
+            <h3 id="greeting">Halo kak, {{ Auth::user()->name }}</h3>
             @else
-            <h3>Halo Guest</h3>
+            <h3 id="greeting">Halo Guest</h3>
             @endif
         </div>
         <ul>
             @if(Auth::check())
-                <li><a href="#" class="btn btn-success">{{ Auth::user()->name }}</a></li>
-                <li><a href="{{ url('/logout') }}" class="btn btn-success">Logout</a></li>
+                <li><a href="{{ url('/logout') }}" class="btn btn-success button-top"><p>logout</p></a></li>
             @else
-                <li><a href="{{ url('/register') }}" class="btn btn-success">Register</a></li>
-                <li><a href="{{ url('/login') }}" class="btn btn-success">Login</a></li>
+                <li><a href="{{ url('/register') }}" class="btn btn-success button-top"><p>Register</p></a></li>
+                <li><a href="{{ url('/login') }}" class="btn btn-success button-top"><p>Login</p></a></li>
             @endif
         </ul>
     </nav>
