96b531c91a96c2dedba6d2b473c66f959bec4bde
Alezuna Nadhif <110968215+AlezunaNadhif@users.noreply.github.com>
2023-06-23 09:44:07 +0700

    ID-Notes

--- resources/views/layouts/app/footer.blade.php
+++ resources/views/layouts/app/footer.blade.php
@@ -1,15 +1,15 @@
 <div class="container">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-        <div class="col-md-4 d-flex align-items-center">
-            <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <svg class="bi" width="30" height="24">
-                    <use xlink:href="#bootstrap"></use>
-                </svg>
-            </a>
-            <span class="mb-3 mb-md-0 text-body-secondary">© 2023 ID-Notes, Inc</span>
-        </div>
+        <p class="col-md-4 mb-0 text-body-secondary">© 2023 ID-Notes, Inc</p>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <a href="/"
+            class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+        </a>
+
+        <ul class="nav col-md-4 justify-content-end">
             <li class="ms-3">
                 <a class="text-body-secondary" href="https://instagram.com/alezunadhif_" target="_blank">
                     <i class="fa-brands fa-instagram"></i>
--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -1,21 +1,45 @@
-<header>
-    <nav>
-        <div id="flex">
-            @if(Auth::check())
-            <h3 id="greeting">Halo kak, {{ Auth::user()->name }}</h3>
+<nav class="py-2 bg-body-tertiary border-bottom">
+    <div class="container d-flex flex-wrap">
+        <ul class="nav me-auto">
+            @if (Auth::check())
+                <li class="nav-item"><a href="{{ url('/note') }}" class="nav-link link-body-emphasis px-2 active"
+                        aria-current="page">Home</a></li>
             @else
-            <h3 id="greeting">Halo Guest</h3>
-            @endif
-        </div>
-        <ul>
-            @if(Auth::check())
-                <li><a href="{{ url('/logout') }}" class="btn btn-success button-top"><p>logout</p></a></li>
-            @else
-                <li><a href="{{ url('/register') }}" class="btn btn-success button-top"><p>Register</p></a></li>
-                <li><a href="{{ url('/login') }}" class="btn btn-success button-top"><p>Login</p></a></li>
+                <li class="nav-item"><a href="{{ url('/') }}" class="nav-link link-body-emphasis px-2 active"
+                        aria-current="page">Home</a></li>
             @endif
         </ul>
-    </nav>
+        <ul class="nav">
+            @if (Auth::check())
+                <li class="nav-item"><a href="3"
+                        class="nav-link link-body-emphasis px-2">{{ Auth::user()->name }}</a></li>
+                <li class="nav-item"><a href="{{ url('/logout') }}" class="nav-link link-body-emphasis px-2">Logout</a>
+                </li>
+            @else
+                <li class="nav-item"><a href="{{ url('/login') }}" class="nav-link link-body-emphasis px-2">Login</a>
+                </li>
+                <li class="nav-item"><a href="{{ url('/register') }}" class="nav-link link-body-emphasis px-2">Sign
+                        up</a></li>
+            @endif
+        </ul>
+    </div>
+</nav>
+
+<header class="py-3 mb-4 border-bottom">
+    <div class="container d-flex flex-wrap justify-content-center">
+        <a href="/"
+            class="d-flex align-items-center mb-3 mb-lg-0 me-lg-auto link-body-emphasis text-decoration-none">
+            <svg class="bi me-2" width="40" height="32">
+                <use xlink:href="#bootstrap"></use>
+            </svg>
+            @if (Auth::check())
+                <span class="fs-4">ID-Notes, kak {{ Auth::user()->name }}</span>
+            @else
+                <span class="fs-4">ID-Notes</span>
+            @endif
+        </a>
+        <form class="col-12 col-lg-auto mb-3 mb-lg-0" role="search">
+            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
+        </form>
+    </div>
 </header>
-
-
