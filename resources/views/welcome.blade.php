75c3ce4381a8007dbd3f3c134c774845a0d76ee8
alezunanadhif <nadhifalezuna@gmail.com>
2023-09-21 21:33:12 +0700

    ID-Notes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Welcome')
 @section('content')
     <div class="position-relative mx-auto my-5 p-5 text-justify text-muted bg-body border border-dashed rounded-5">
-        <img src="{{ asset("assets/img/logo.svg")}}" alt="ID-Notes">
+        <img src="{{ asset("assets/img/logo.svg")}}" alt="ID-Notes" class="mt-5">
         <h4 class="col-lg-6 mb-4">
             take notes without hassle, save your notes on the web, and don't forget it's all free for you!
         </h4>
