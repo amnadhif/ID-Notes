a4048ec65430178a0d5696547885020784c4cf06
alezunanadhif <nadhifalezuna@gmail.com>
2023-09-22 20:56:58 +0700

    ID-Notes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -12,7 +12,7 @@ class AuthController extends Controller
 {
     public function login()
     {
-        if(Auth::check()){
+        if(Auth::check()) {
             return redirect('/note');
         }
 
