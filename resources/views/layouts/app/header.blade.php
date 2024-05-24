87cda82ba2ece9d83fd93454e00fd43622af023b
amnadhif <pohannadhif@gmail.com>
2024-05-25 00:05:31 +0300

    ID-Notes

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -2,18 +2,30 @@
     <div class="container d-flex flex-wrap">
         <ul class="nav me-auto">
             @if (Auth::check())
-                <li class="nav-item"><a href="{{ url('/note') }}" class="nav-link link-body-emphasis px-2 active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="{{ url('/note') }}" class="nav-link link-body-emphasis px-2 active"
+                    aria-current="page">Home</a></li>
             @else
-                <li class="nav-item"><a href="{{ url('/') }}" class="nav-link link-body-emphasis px-2 active" aria-current="page">Home</a></li>
+            <li class="nav-item"><a href="{{ url('/') }}" class="nav-link link-body-emphasis px-2 active"
+                    aria-current="page">Home</a></li>
             @endif
         </ul>
         <ul class="nav">
             @if (Auth::check())
-                <li class="nav-item"><a href="{{ url('/profile') }}" class="nav-link link-body-emphasis px-2">{{ Auth::user()->name }}</a></li>
-                {{-- <li class="nav-item"><a href="{{ url('/logout') }}" class="nav-link link-body-emphasis px-2">Logout</a></li> --}}
+            <li class="nav-item d-flex align-items-center">
+                <a href="{{ url('/profile') }}" class="nav-link link-body-emphasis px-2 d-flex align-items-center">
+                    <div class="initials"
+                        style="width: 30px; height: 30px; border-radius: 50%; background-color: #ccc; color: #fff; display: flex; align-items: center; justify-content: center; margin-right: 10px;">
+                        {{ strtoupper(substr(Auth::user()->name, 0, 1)) }}
+                    </div>
+                    {{ Auth::user()->name }}
+                </a>
+            </li>
+            {{-- <li class="nav-item"><a href="{{ url('/logout') }}" class="nav-link link-body-emphasis px-2">Logout</a>
+            </li> --}}
             @else
-                <li class="nav-item"><a href="{{ url('/login') }}" class="nav-link link-body-emphasis px-2">Login</a></li>
-                <li class="nav-item"><a href="{{ url('/register') }}" class="nav-link link-body-emphasis px-2">Sign up</a></li>
+            <li class="nav-item"><a href="{{ url('/login') }}" class="nav-link link-body-emphasis px-2">Login</a></li>
+            <li class="nav-item"><a href="{{ url('/register') }}" class="nav-link link-body-emphasis px-2">Sign up</a>
+            </li>
             @endif
         </ul>
     </div>
